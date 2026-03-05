a09377f937549d0c4c150b28984eb22b21c9657a
edsouza02 <edsouza2@minha.fag.edu.br>
2026-03-04 21:46:30 -0300

    Criado o Hello World em Alemão

--- Hello.java
+++ Hello.java
@@ -1,9 +1,15 @@
 public class Hello {
 	public static void main(String[] args) {
 		helloworld();
+		Alemon.hello();
 	}
 	
 	public static void helloworld() {
 		System.out.println("Hello World");
+	}
+}
+class Alemon{
+	public static void hello() {
+		System.out.println("Hallo Welt!");
 	}
 }
